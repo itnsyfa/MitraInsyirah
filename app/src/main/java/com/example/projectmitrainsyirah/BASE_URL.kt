73af8edfe154f4da8a320690f057be346ad8f860
itnsyfa <intan20si@mahasiswa.pcr.ac.id>
2024-12-10 21:33:24 +0700

    Initial commit

--- app/src/main/java/com/example/projectmitrainsyirah/BASE_URL.kt
+++ app/src/main/java/com/example/projectmitrainsyirah/BASE_URL.kt
@@ -1,5 +1,5 @@
 package com.example.projectmitrainsyirah
 
 object BASE_URL {
-        const val NGROK_URL: String = "https://8ba9-2001-448a-1082-c519-6d7f-f51d-58d6-de13.ap.ngrok.io/api/"
+        const val NGROK_URL: String = "https://020c-125-165-104-145.ap.ngrok.io/api/"
     }
